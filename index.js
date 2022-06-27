0d97971b4254d02e8dfd5bbdbef3f336982b1d98
hugomgwtf <hugomgwtf@outlook.com>
2022-06-27 11:08:08 +0200

    temporary SPA fix

--- index.js
+++ index.js
@@ -4,22 +4,29 @@
 // @version      0.1
 // @description  Automatically redirects to the https://youtube.com/watch?v={id} page when opening a youtube shorts page
 // @author       hugomgwtf
-// @match        *://*.youtube.com/shorts/*
-// @match        *://*.youtube.co/shorts/*
-// @match        *://*.youtube.co.uk/shorts/*
-// @match        *://*.youtube.co.nz/shorts/*
-// @match        *://*.youtube.be/shorts/*
-// @match        *://*.youtube.fr/shorts/*
-// @match        *://*.youtube.nl/shorts/*
-// @match        *://*.youtu.be/shorts/*
+// @match        *://*.youtube.com/*
+// @match        *://*.youtube.co/*
+// @match        *://*.youtube.co.uk/*
+// @match        *://*.youtube.co.nz/*
+// @match        *://*.youtube.be/*
+// @match        *://*.youtube.fr/*
+// @match        *://*.youtube.nl/*
+// @match        *://*.youtu.be/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    const url = new URL(location);
-    const match = /^\/shorts\/(.*?)$/gi.exec(url.pathname);
-    if(null !== match)
-        window.location = `https://${url.hostname}/watch?v=${match[1]}`;
+
+    function redirectIfShorts() {
+        const url = new URL(location);
+        const match = /^\/shorts\/(.*?)$/gi.exec(url.pathname);
+        if(null !== match)
+            window.location = `https://${url.hostname}/watch?v=${match[1]}`;
+    }
+
+    window.addEventListener('mouseup', function () {
+        setTimeout(() => redirectIfShorts(), 150);
+    });
 })();
